043671db0e43a07a0ce64b5bb61a2daaefd35450
juansebastianpabon <nova159712@gmail.com>
2025-12-12 09:24:07 -0500

    Añade la homepage y una mejora al layout

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import Navigation from '@/app/_components/Navigation';
 import Logo from '@/app/_components/Logo';
 import '@/app/_styles/globals.css';
 import { Josefin_Sans } from 'next/font/google';
+import Header from './_components/Header';
 const josefinn = Josefin_Sans({
   subsets: ['latin'],
   display: 'swap',
@@ -20,14 +21,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body
-        className={`bg-primary-950 text-primary-100 min-h-screen ${josefinn.className}`}
+        className={`bg-primary-950 text-primary-100 min-h-screen flex flex-col  ${josefinn.className}`}
       >
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <Header />
+        <div className='flex-1 px-8 py-12 '>
+          <main className='max-w-7xl mx-auto'>{children}</main>
+        </div>
       </body>
     </html>
   );
