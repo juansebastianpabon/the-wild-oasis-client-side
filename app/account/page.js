260220d2bd4d2479d6f2b957913ecc652f18c871
juansebastianpabon <nova159712@gmail.com>
2025-12-12 11:59:33 -0500

    Añade las rutas faltantes asi como sus layouts y rutas anidadas

--- app/account/page.js
+++ app/account/page.js
@@ -1,7 +1,12 @@
-export const metadata = {
-  title: 'Account ',
-};
+import ReservationCard from '@/app/_components/ReservationCard';
 
-export default function Account() {
-  return <h1>Account Page</h1>;
+export default function Page() {
+  // CHANGE
+  const bookings = [];
+
+  return (
+    <h2 className='font-semibold text-2xl text-accent-400 mb-7'>
+      Welcome to your account area
+    </h2>
+  );
 }
